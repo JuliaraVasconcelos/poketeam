d475731cc484bf0901b6ca6a24497a2e0fe9b1c1
Juliara Vasconcelos <40279322+JuliaraVasconcelos@users.noreply.github.com>
2022-09-30 11:44:27 -0300

    att 30/09

--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
 export const searchPokemon = async (pokemon) => {
-    try{
+    try {
+        // var url = "";
+
+        // if (pokemon === 0) {
+        //     url = "https://pokeapi.co/api/v2/pokemon?limit=1000"
+        // } else {
+        //     url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        // }
+        
         let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
         const response = await fetch(url)
         return await response.json()
     }
-    catch (error){
+    catch (error) {
         console.log('error', error);
     }
 }
--- src/components/Seachbar.js
+++ src/components/Seachbar.js
@@ -5,33 +5,20 @@ import { searchPokemon } from "../api";
 const Searchbar = () => {
     const [search, setSearch] = useState('ditto')
     const [pokemon, setPokemon] = useState()
-    const [random, setRandom] = useState(0)
-
-    
+    const [random, setRandom] = useState('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.')
 
     //gera número aleatório entre a quantidade de pokemon existentes
     const onRandomPoke = () =>{
         let min = Math.ceil(1);
         let max = Math.floor(905);
-        let rand = Math.floor(Math.random() * (max - min) + min);
+        let rand = Math.floor(Math.random() * (max - min) + 1);
         return rand
     }
-
+    //chama o pokemon na api baseado no número que saiu na função anterior
     const onClickButton = () => {
         setRandom(onRandomPoke)
-        // onSearchHandler(rand)
-        // console.log('pokemon:', rand);
-    }
-
-    //busca o pokemon baseado no número/nome digitado
-    const onChangeHandler = (e) => {
-        console.log('pokemon:', e.target.value)
-        setSearch(e.target.value)
-    }
-
-    const onButtonClickHandler = () => {
-        onSearchHandler(search)
-        console.log('pokemon:', search);
+        onSearchHandler(random)
+        console.log('pokemon:', random);
     }
 
     const onSearchHandler = async (pokemon) => {
@@ -39,27 +26,43 @@ const Searchbar = () => {
         setPokemon(result)
     }
     
+    //adiciona o pokemon sorteado à equipe
+    const addToTeam = () => {
+
+    }
+
+    //nega o pokemon sorteado
+    const dontAdd = () => {
+
+    }
+    //busca o pokemon baseado no número/nome digitado
+    // const onChangeHandler = (e) => {
+    //     console.log('pokemon:', e.target.value)
+    //     setSearch(e.target.value)
+    // }
+
+    // const onButtonClickHandler = () => {
+    //     onSearchHandler(search)
+    //     console.log('pokemon:', search);
+    // }
+    
     return (
         <div className="searchbar-container">
-            <div className="searchbar">
-                <input placeholder="buscar pokemon" onChange={onChangeHandler} />
-            </div>
             <div className="searchbar-btn">
-                <button onClick={onButtonClickHandler}>Sortear novo Pokémon</button>
-            </div>
-            <div className="searchbar">
-                {random}
-            </div>
-            <div className="searchbar-btn">
-                <button onClick={onClickButton}>Poke aleatório</button>
+                <button onClick={onClickButton}>Sortear Pokémon</button>
             </div>
             {pokemon ? (
                 <div>
-                    <div>nome: {pokemon.name}</div>
-                    <div>peso: {pokemon.weight}</div>
                     <img src={pokemon.sprites.front_default} alt="{pokemon.name}"/>
+                    <div>Número: {pokemon.id}</div>
+                    <div>Nome: {pokemon.name}</div>
+                    <div>Peso: {pokemon.weight}</div>
                 </div>
             ): null}
+            <div>Adicionar Pokemon à equipe?</div>
+            <div className="searchbar-btn">
+                <button onClick={addToTeam}>Sim</button> <button onClick={dontAdd}>Não</button>
+            </div>
         </div>
     )
 }
